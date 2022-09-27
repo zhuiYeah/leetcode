c3dce9af4dc6fe6dca9a83ce629064eb3b15c09f
kgao <850013957@qq.com>
2022-09-27 17:05:37 +0800

    Rename dfs 回溯 *黄金矿工 单词搜索.go to dfs 回溯 _黄金矿工 单词搜索.go

---
+++ 太简单了没意思/多个数组求交集.java
@@ -0,0 +1,32 @@
+package 太简单了没意思;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class 多个数组求交集 {
+    public List<Integer> intersection(int[][] nums) {
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums[0]) {
+            set.add(i);
+        }
+        for (int i = 1; i < nums.length; i++) {
+            var copy = new HashSet<Integer>();
+            for (int j : nums[i]) {
+                if (set.contains(j)) {
+                    copy.add(j);
+                }
+            }
+            if (copy.size() == 0) return result;
+            set.clear();
+            set.addAll(copy);
+        }
+        result.addAll(set);
+        result.sort((a, b) -> {
+            return a - b;
+        });
+        return result;
+    }
+}
