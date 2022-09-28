6ff48aacdc51832a646283e50a450944893fd9ef
kgao <850013957@qq.com>
2022-09-28 16:45:33 +0800

    Rename dfs 回溯 *黄金矿工 单词搜索.go to dfs 回溯 _黄金矿工 单词搜索.go

--- 排序/堆_第k个数.java
+++ 排序/堆_第k个数.java
@@ -27,25 +27,25 @@ public class 堆_第k个数 {
         }
     }
 
-    public int getKthMagicNumber(int k) {
-        int[] factors = {3, 5, 7};
-        Set<Long> seen = new HashSet<Long>();
-        PriorityQueue<Long> heap = new PriorityQueue<Long>();
-        seen.add(1L);
-        heap.offer(1L);
-        int magic = 0;
-        for (int i = 0; i < k; i++) {
-            long curr = heap.poll();
-            magic = (int) curr;
-            for (int factor : factors) {
-                long next = curr * factor;
-                if (seen.add(next)) {
-                    heap.offer(next);
-                }
-            }
-        }
-        return magic;
-    }
+//    public int getKthMagicNumber(int k) {
+//        int[] factors = {3, 5, 7};
+//        Set<Long> seen = new HashSet<Long>();
+//        PriorityQueue<Long> heap = new PriorityQueue<Long>();
+//        seen.add(1L);
+//        heap.offer(1L);
+//        int magic = 0;
+//        for (int i = 0; i < k; i++) {
+//            long curr = heap.poll();
+//            magic = (int) curr;
+//            for (int factor : factors) {
+//                long next = curr * factor;
+//                if (seen.add(next)) {
+//                    heap.offer(next);
+//                }
+//            }
+//        }
+//        return magic;
+//    }
 
 
 }
