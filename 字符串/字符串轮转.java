c19b67279223f9bcd0029ade955c23e5a72450c7
kgao <850013957@qq.com>
2022-09-29 18:42:01 +0800

    Rename dfs 回溯 *黄金矿工 单词搜索.go to dfs 回溯 _黄金矿工 单词搜索.go

--- 字符串/字符串轮转.java
+++ 字符串/字符串轮转.java
@@ -1,5 +1,7 @@
 package 字符串;
 
+//s1 旋转后 变为 s2
+//那么 s1+s1 一定包含 s2
 public class 字符串轮转 {
     public boolean isFlipedString(String s1, String s2) {
         return (s1.length() == s2.length() && (s1 + s1).contains(s2));
