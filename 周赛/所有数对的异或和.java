dd997dc9bfbde4a1c255e58a30317ec33999ac76
kgao <850013957@qq.com>
2022-10-02 10:25:43 +0800

    Rename dfs 回溯 *黄金矿工 单词搜索.go to dfs 回溯 _黄金矿工 单词搜索.go

--- 周赛/所有数对的异或和.java
+++ 周赛/所有数对的异或和.java
@@ -1,5 +1,6 @@
 package 周赛;
 
+//这题确实不会
 //这里超时
 public class 所有数对的异或和 {
     public int xorAllNums(int[] nums1, int[] nums2) {
@@ -8,6 +9,22 @@ public class 所有数对的异或和 {
             for (int j = 0; j < nums2.length; j++) {
                 res ^= nums1[j] ^ nums2[j];
             }
+        }
+        return res;
+    }
+}
+
+
+
+class dwecweq{
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int res = 0;
+        int m = nums1.length , n = nums2.length;
+        if (m % 2 == 1) {
+            for (int num : nums2)  res ^= num;
+        }
+        if (n % 2 == 1) {
+            for (int num : nums1)  res ^= num;
         }
         return res;
     }
--- 周赛/满足不等式的数对数目.java
+++ 周赛/满足不等式的数对数目.java
@@ -1,5 +1,6 @@
 package 周赛;
 
+//没时间做了
 public class 满足不等式的数对数目 {
 //    public long numberOfPairs(int[] nums1, int[] nums2, int diff) {
 //
