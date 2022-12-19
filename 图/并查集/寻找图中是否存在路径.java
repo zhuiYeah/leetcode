09b7832e55771ffbf1dab911568a4546380310f0
zhuiYeah <850013957@qq.com>
2022-12-19 14:21:58 +0800

    Changes

--- 图/并查集/寻找图中是否存在路径.java
+++ 图/并查集/寻找图中是否存在路径.java
@@ -40,3 +40,43 @@ public class 寻找图中是否存在路径 {
     }
 
 }
+
+
+//每日一题再写一次并查集
+class dede {
+    int[] father;
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        initialize(n);
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+            merge(a, b);
+        }
+        return isFamily(source, destination);
+    }
+
+    private void initialize(int size) {
+        father = new int[size];
+        for (int i = 0; i < father.length; i++) father[i] = i;
+    }
+
+    private int find(int x) {
+        if (x == father[x]) return x;
+        father[x] = find(father[x]);
+        return father[x];
+    }
+
+    private void merge(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) return;
+        father[a] = b;
+    }
+
+    private boolean isFamily(int a, int b) {
+        a = find(a);
+        b = find(b);
+        return a == b;
+    }
+
+}
